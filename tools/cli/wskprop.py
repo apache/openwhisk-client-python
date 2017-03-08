9c4e80bcad372b4cbf0b36f961660f457dc98894
cclauss <cclauss@bluewin.ch>
2017-03-08 01:53:28 +0100

    Update wskprop.py

--- tools/cli/wskprop.py
+++ tools/cli/wskprop.py
@@ -40,36 +40,30 @@ def propfile(base):
 
 
 def importPropsIfAvailable(filename):
-    thefile = open(filename, 'r') if os.path.isfile(filename) and os.path.exists(filename) else []
+    thefile = open(filename, 'r') if (os.path.isfile(filename) and
+                                      os.path.exists(filename)) else []
     return importProps(thefile)
 
 
 def importDefaultProps():
     packagename = 'whisk'
     filename = 'default.props'
-    filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
-    theFile = open(filepath, 'r') if os.path.isfile(filepath) and os.path.exists(filepath) else None
-    if theFile is None:
+    filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                            filename)
+    theFile = open(filepath, 'r') if (os.path.isfile(filepath) and
+                                      os.path.exists(filepath)) else None
+    if not theFile:
         try:
             theFile = pkg_resources.resource_stream(packagename, filename)
         except ImportError:
             theFile = None
-    return importProps(theFile) if theFile is not None else None
+    return importProps(theFile) if theFile else None
 
 
 def importProps(stream):
-    props = {}
-    for line in stream:
-        parts = line.split('=')
-        if len(parts) >= 1:
-            key = parts[0].strip()
-        if len(parts) >= 2:
-            val = parts[1].strip()
-        if key != '' and val != '':
-            props[key.upper().replace('.','_')] = val
-        elif key != '':
-            props[key.upper().replace('.','_')] = ''
-    return props
+    return {key.strip().upper().replace('.', '_'): val.strip()
+            for key, _, val in (line.partition('=') for line in stream)
+            if key.strip()}
 
 
 def updateProps(key, value, filename):
@@ -79,24 +73,24 @@ def updateProps(key, value, filename):
 
 
 def writeProps(props, filename):
-    fileHandle = open(filename, 'w')
-    for key in props:
-        line = key.upper() + '=' + props[key]
-        fileHandle.write(line + '\n')
-    fileHandle.close()
+    with open(filename, 'w') as fileHandle:
+        for key, val in props.items():
+            fileHandle.write(key.upper() + '=' + val + '\n')
 
 
 #
-# Returns a triple of (length(requiredProperties), requiredProperties, deferredInfo)
-# prints a message if a required property is not found
-#
+# Returns a triple of (length(requiredProperties), requiredProperties,
+# deferredInfo)  Prints a message if a required property is not found
 def checkRequiredProperties(requiredPropertiesByName, properties):
-    requiredPropertiesByValue = [ getPropertyValue(key, properties) for key in requiredPropertiesByName ]
-    requiredProperties = dict(zip(requiredPropertiesByName, requiredPropertiesByValue))
-    invalidProperties = [ key for key in requiredPropertiesByName if requiredProperties[key] == None ]
+    requiredPropertiesByValue = [getPropertyValue(key, properties) for key
+                                 in requiredPropertiesByName]
+    requiredProperties = dict(zip(requiredPropertiesByName,
+                                  requiredPropertiesByValue))
+    invalidProperties = [key for key in requiredPropertiesByName
+                         if requiredProperties[key] is None]
     deferredInfo = ''
     for key, value in requiredProperties.items():
-        if not value == None or value == '':
+        if value is not None or value == '':
             print('property "%s" not found in environment or property file' %
                   key)
         else:
@@ -106,9 +100,7 @@ def checkRequiredProperties(requiredPropertiesByName, properties):
 
 
 def getPropertyValue(key, properties):
-    evalue = os.environ.get(key)
-    value  = evalue if evalue is not None and evalue != '' else properties[key] if key in properties else None
-    return value
+    return os.environ.get(key) or properties.get(key)
 
 
 def merge(props1, props2):
